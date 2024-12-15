0c01bb38d471843816098df53bb87b7cb5975e15
TienDung0312 <110339623+ntd0@users.noreply.github.com>
2024-12-15 20:54:40 +0700

    add code YearlyRevenueResponse

--- app/src/main/java/com/example/shoe/models/YearlyRevenueResponse.java
+++ app/src/main/java/com/example/shoe/models/YearlyRevenueResponse.java
@@ -1,4 +1,16 @@
 package com.example.shoe.models;
 
+import java.util.List;
+
 public class YearlyRevenueResponse {
+    private int year;
+    private List<Integer> revenue;
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<Integer> getRevenue() {
+        return revenue;
+    }
 }
