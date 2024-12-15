8e4d4464394d1e9539df25dccf70c14d2ce800f9
TienDung0312 <110339623+ntd0@users.noreply.github.com>
2024-12-15 20:27:24 +0700

    add VoucherManagement

---
+++ app/src/main/java/com/example/shoe/ui/voucherManagement/VoucherManagementActivity.java
@@ -0,0 +1,16 @@
+package com.example.shoe.ui.voucherManagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.shoe.R;
+
+public class VoucherManagementActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voucher_management);
+
+    }
+}
