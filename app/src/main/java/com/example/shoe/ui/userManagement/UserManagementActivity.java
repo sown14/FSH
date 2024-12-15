9112fb3725d0cff6d63352f11bed82f43f0bcd73
TienDung0312 <110339623+ntd0@users.noreply.github.com>
2024-12-15 20:37:25 +0700

    create UserManagementActivity

---
+++ app/src/main/java/com/example/shoe/ui/userManagement/UserManagementActivity.java
@@ -0,0 +1,16 @@
+package com.example.shoe.ui.userManagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.shoe.R;
+
+public class UserManagementActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_management);
+
+    }
+}
