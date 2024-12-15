7b5d73f3dd93bfe54a2bec1aa39fbf5fe818d03a
TienDung0312 <110339623+ntd0@users.noreply.github.com>
2024-12-15 20:22:48 +0700

    add code Voucher

--- app/src/main/java/com/example/shoe/models/Voucher.java
+++ app/src/main/java/com/example/shoe/models/Voucher.java
@@ -1,4 +1,94 @@
 package com.example.shoe.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Voucher {
+
+    @SerializedName("code")
+    private String code;
+
+    @SerializedName("description")
+    private String description;
+
+    @SerializedName("min")
+    private int min;
+
+    @SerializedName("max")
+    private int max;
+
+    @SerializedName("limit")
+    private int limit;
+
+    @SerializedName("timesUsed")
+    private int timesUsed;
+
+    @SerializedName("minOrderValue")
+    private int minOrderValue;
+
+    @SerializedName("discount")
+    private int discount;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public int getTimesUsed() {
+        return timesUsed;
+    }
+
+    public void setTimesUsed(int timesUsed) {
+        this.timesUsed = timesUsed;
+    }
+
+    public int getMinOrderValue() {
+        return minOrderValue;
+    }
+
+    public void setMinOrderValue(int minOrderValue) {
+        this.minOrderValue = minOrderValue;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
 }
